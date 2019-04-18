5408c3af57352032270ad55198052991b1178ed3
byWhish <lareugerardo@gmail.com>
2019-04-18 18:32:00 -0300

    feat(Persistence): kick-start with entities and repositories

--- src/main/java/unq/tpi/desapp/App.java
+++ src/main/java/unq/tpi/desapp/App.java
@@ -2,12 +2,14 @@ package unq.tpi.desapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableJpaRepositories("unq.tpi.dessap")
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
--- src/main/java/unq/tpi/desapp/persistence/UserRepo.java
+++ src/main/java/unq/tpi/desapp/persistence/UserRepo.java
@@ -1,7 +1,16 @@
 package unq.tpi.desapp.persistence;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import unq.tpi.desapp.model.User;
 
-public interface UserRepo extends CrudRepository<User, Long>{}
+import java.util.Optional;
 
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {
+
+    @Query("SELECT u.title FROM User u where u.email = :email")
+    Optional<User> findUserByEmail(@Param("email") Long email);
+}
