26360e80c51741765a144eb0b291958f08b5b604
Gaston <frutos.gaston@gmail.com>
2019-06-25 23:10:09 -0300

    logeo con parametros y usuario. se logea en archivo

--- src/main/java/unq/tpi/desapp/aspect/LoggingAspect.java
+++ src/main/java/unq/tpi/desapp/aspect/LoggingAspect.java
@@ -1,23 +1,32 @@
 package unq.tpi.desapp.aspect;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+    private final static Logger LOGGER = Logger.getLogger(LoggingAspect.class);
 
     @Pointcut("execution(* unq.tpi.desapp.webservice..*(..))")
     private void loggingTargets() {}
 
     @Before("loggingTargets()")
     public void logEnterMethod(JoinPoint thisJoinPoint) {
-        LOGGER.info("ENTER " + thisJoinPoint);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        ObjectMapper objectMapper = new ObjectMapper();
+        LOGGER.info("USER: " + username + " ENTER " + thisJoinPoint);
+        try {
+            LOGGER.info("WITH ARGS: " + objectMapper.writeValueAsString(thisJoinPoint.getArgs()));
+        } catch (JsonProcessingException e) {
+            LOGGER.warn("error while logging args", e);
+        }
     }
 
     @AfterReturning(pointcut = "loggingTargets()", returning = "result")
